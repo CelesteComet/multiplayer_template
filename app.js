7428d727953a4e4a64a85d4f76422589c7f41814
CelesteComet <brucewong21@gmail.com>
2017-11-01 23:46:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ io.on('connection', function(socket){
 });
 
 // Handle environment changes
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || 8080;
+
  
-http.listen(port, ip_address, function(){
+http.listen(port, function(){
     console.log( "Listening on " + ip_address + ", server_port " + port );
 });
 
